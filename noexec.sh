84a0c36a016c5e3d7562251981fe6189cad3583f
U-ALIONSCIENCE\ESmith-rowland <ESmith-rowland@SEG_SMTHRWLNDLT.alionscience.com>
2015-06-03 17:58:39 -0400

    Add a noexec.sh

---
+++ noexec.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+chmod -R -f +rw,-x \
+ *.cpp *.tpp *.h *.for *.cs \
+ *.dat *.DAT *.txt *.in *.out \
+ *.jpg *.png *.ico *.ttf *.iv \
+ .gitignore \
+ *.gz *.bz2 *.zip \
+ Makefile Makefile.* Doxy* \
+ README TODO \
+ *.pdf *.doc *.ppt *.xls
+
+ chmod -f +rwx *.sh
+ 
